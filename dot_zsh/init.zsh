6296ce87dbfa2e778b8b37b0680edbe76e4f1a69
kinjo <kinjo.bobchin@gmail.com>
2026-07-31 10:39:08 +0900

    improve fzf-tab

--- dot_zsh/init.zsh
+++ dot_zsh/init.zsh
@@ -10,6 +10,54 @@ if command -v fzf >/dev/null 2>&1 && [[ -o interactive ]]; then
     compinit
   fi
 
+  if (( $+commands[docker] )) && (( ! $+functions[_docker] )); then
+    source <(docker completion zsh)
+  fi
+
+  if (( $+commands[docker-compose] )) && (( $+functions[_docker] )); then
+    _docker_compose_native_completion() {
+      local -a response candidates
+      local output line candidate description directive=0
+
+      output=$(docker-compose __complete "${words[3,CURRENT]}" 2>/dev/null)
+      response=("${(@f)output}")
+
+      if [[ ${response[-1]} == :<-> ]]; then
+        directive=${response[-1]#:}
+        response[-1]=()
+      fi
+
+      for line in "${response[@]}"; do
+        if [[ $line == *$'\t'* ]]; then
+          candidate=${line%%$'\t'*}
+          description=${line#*$'\t'}
+          candidates+=("${candidate//:/\\:}:${description}")
+        else
+          candidates+=("${line//:/\\:}")
+        fi
+      done
+
+      if (( ${#candidates} )); then
+        if (( directive & 2 )); then
+          _describe 'docker compose value' candidates -- -S ''
+        else
+          _describe 'docker compose value' candidates
+        fi
+      elif (( !(directive & 4) )); then
+        _files
+      fi
+    }
+
+    _docker_with_compose_completion() {
+      if [[ ${words[2]} == compose ]]; then
+        _docker_compose_native_completion
+      else
+        _docker
+      fi
+    }
+    compdef _docker_with_compose_completion docker
+  fi
+
   if [[ -r /opt/homebrew/opt/fzf-tab/share/fzf-tab/fzf-tab.zsh ]]; then
     zstyle ':completion:*' menu no
     zstyle ':fzf-tab:*' fzf-command fzf
